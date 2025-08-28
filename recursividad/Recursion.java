21989f98fedb1d060f522421a301bc499902ce92
ktorresagui <al03022345@tecmilenio.mx>
2025-08-28 15:00:02 -0600

    Update Recursion.java

--- recursividad/Recursion.java
+++ recursividad/Recursion.java
@@ -2,7 +2,7 @@ public class Recursion {
 
     // Factorial recursivo
     public static long factorial(long n){
-        if (n <= 1){   // Caso base
+        if (n <= 1){
             return 1;
         } else {
             return n * factorial(n-1);
@@ -11,7 +11,7 @@ public class Recursion {
 
     // Fibonacci recursivo
     public static long fibonacci(long n){
-        if (n <= 1){   // Caso base
+        if (n <= 1){
             return 1;
         } else {
             return fibonacci(n-1) + fibonacci(n-2);
@@ -20,19 +20,30 @@ public class Recursion {
 
     // Suma de dígitos recursiva
     public static int sumaDigitos(int n){
-        if (n < 10){   // Caso base: un solo dígito
+        if (n < 10){
             return n;
         } else {
-            return (n % 10) + sumaDigitos(n / 10);  // Separa último dígito y llama recursivamente
+            return (n % 10) + sumaDigitos(n / 10);
         }
     }
 
     // Invertir cadena recursiva
     public static String invertirCadena(String str){
-        if (str.isEmpty()){  // Caso base
+        if (str.isEmpty()){
             return str;
         } else {
             return invertirCadena(str.substring(1)) + str.charAt(0);
+        }
+    }
+
+    // Conteo de vocales recursivo
+    public static int contarVocales(String str){
+        if (str.isEmpty()){
+            return 0;
+        } else {
+            char c = Character.toLowerCase(str.charAt(0));
+            int suma = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') ? 1 : 0;
+            return suma + contarVocales(str.substring(1));
         }
     }
 }
